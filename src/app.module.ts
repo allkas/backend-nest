8926c2c20c6ba90f950ac0440458403d775b2fe7
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2022-01-12 15:42:13 +0300

    конфиг и подключение бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersController } from "./users/users.controller";
 
 @Module({
-    controllers: [AppController]
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRESS_HOST,
+            port: Number(process.env.POSTGRESS_PORT),
+            username: process.env.POSTGRESS_USER,
+            password: process.env.POSTGRESS_PASSWORD,
+            database: process.env.POSTGRESS_DB,
+            models: [],
+            autoLoadModels: true
+        }),
+        UsersController,
+    ]
 })
 export class AppModule {}
